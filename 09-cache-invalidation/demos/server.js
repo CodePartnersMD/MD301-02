7edbc8cdbf54a5bda4775943b75e890daf7eb885
zdiehlio <zachary.diehl@gmail.com>
2018-12-06 11:43:21 -0500

    pushing demo for lecture 9

---
+++ 09-cache-invalidation/demos/server.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const express = require('express')
+const superagent = require('superagent')
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+//same thing as above
+// const thisSchema = mongoose.Schema
+
+
+require('dotenv').config()
+
+const mongoURL = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@ds135537.mlab.com:35537/md301-server`
+
+mongoose.connect(mongoURL)
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+
+db.once('open', () => {
+  console.log('DB connection open!')
+})
+
+const app = express()
+
+
+const cors = require('cors')
+
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+
+app.get('/location', locationController)
+app.get('/weather', weatherController)
+app.get('/', (req, res) => {
+  res.send('<div>This is the Home Route</div>')
+})
+
+app.use('*', (req, res) => {
+  res.send('<img src="https://http.cat/404" />')
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
+
+const LocationSchema = new Schema({
+  address: String,
+  lat: Number,
+  lng: Number
+})
+
+const Location = model('Location', LocationSchema)
+
+function locationController(req, res) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${process.env.GOOGLE_API_KEY}`
+  Location.findOne({ address: req.query.address}, (err, addr) => {
+    if(addr) {
+      console.log('address found', req.query)
+      res.send(addr)
+    } else {
+      superagent.get(url)
+        .then(result => {
+          const newLocation = new Location({
+            address: req.query.address,
+            lat: result.body.results[0].geometry.location.lat,
+            lng: result.body.results[0].geometry.location.lng
+          })
+          newLocation.save()
+          console.log('created new address')
+          res.send(newLocation)
+        })
+    }
+  })
+    .catch(err => res.send('Got an error'))
+}
+
+const WeatherConstructor = function(weather) {
+  this.time = weather.body.currently.time
+  this.summary = weather.body.currently.summary
+  this.icon = weather.body.currently.icon
+  this.temp = weather.body.currently.temperature
+} 
+
+function weatherController(req, res) {
+  const url =`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${req.query.lat},${req.query.lng}`
+
+  superagent.get(url)
+    .then(result => {
+      const newWeather = new WeatherConstructor(result)
+      res.send(newWeather)
+    })
+}
